0de3bc9046a02885fcae059cb8e1ece4f0ca3321
metutam <138866899+metutam@users.noreply.github.com>
2023-07-20 19:33:35 +0300

    Create CalculateTheBikeRentalFee.m

---
+++ matlab-exercises/CalculateTheBikeRentalFee.m
@@ -0,0 +1,16 @@
+%{
+Calculate Rental Fee Of A Bike
+%}
+
+clc;
+clear;
+
+minutes = input('Please Enter How Long Have You Rented The Bike (Minutes): ');
+
+if minutes <= 0
+    fprintf('Please Enter A Valid Value. \n');
+elseif 0 < minutes && minutes < 30
+    fprintf('Rental Fee: 5TL \n');
+else
+    fprintf('Rental Fee: %g TL \n', 5 + (minutes - 30) * 0.25);
+end
